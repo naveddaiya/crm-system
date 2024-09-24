247897c45154ac1e7b8dfebd4b32a9fb36e80c01
Naved <daiyanaved3@gmail.com>
2024-09-24 12:29:16 +0530

    Update  Task

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -27,3 +27,19 @@ exports.getTasks = async(req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+// update task for a user
+exports.updateTasks = async(req, res) => {
+  const taskId = req.params.id
+  const updates = req.body
+  try {
+    let task = await Task.find({_id:taskId, user: req.user._id });
+
+    if(!task){
+      res.status(404).json({message:"Task not found or not authorized to update"})
+    }
+    task = await Task.findByIdAndUpdate(taskId,updates,{new:true,runValidators:true})
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- Routes/tasksRoute.js
+++ Routes/tasksRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createTask, getTasks } = require('../Controller/taskController');
+const { createTask, getTasks, updateTasks } = require('../Controller/taskController');
 const { protect } = require('../Middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', protect, createTask);
 router.get('/', protect, getTasks);
+router.put('/:id', protect, updateTasks);
 
 module.exports = router;
